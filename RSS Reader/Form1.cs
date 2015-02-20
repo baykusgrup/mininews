f7c5874296e0d899af0f3a28cc3f8d80e10d5c54
serbayacar <serbayacar@gmail.com>
2015-02-20 23:29:06 +0200

    ++

--- RSS Reader/Form1.cs
+++ RSS Reader/Form1.cs
@@ -83,7 +83,7 @@ namespace RSS_Reader
 
         private void lstNews_SelectedIndexChanged(object sender, EventArgs e)
         {
-            // When an items is selected
+            // When an items is selected // hi
             if (lstNews.SelectedItems.Count == 1)
             {
                 // Loop through all the nodes under <channel>
